d0f0d46b3e7c70edd6394853549335b5fa135df0
anders nygaard <andeny@statoil.com>
2016-03-15 14:05:25 +0100

    Added license header to javascript

--- scripts/jquery.dropdownbutton.js
+++ scripts/jquery.dropdownbutton.js
@@ -1,3 +1,16 @@
+// jquery.dropdownbutton is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License. See http://creativecommons.org/licenses/by-sa/3.0/
+//
+// You are free:
+// - to Share — to copy, distribute and transmit the work
+// - to Remix — to adapt the work
+// - to make commercial use of the work 
+//
+// Under the following conditions:
+// - Attribution — You must attribute the work in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work).
+// - Share Alike — If you alter, transform, or build upon this work, you may distribute the resulting work only under the same or similar license to this one. 
+//
+// https://twitter.com/#!/anders_nygaard
+
 (function ($) {
     "use strict";
     $.fn.dropdownButton = function (options) {
